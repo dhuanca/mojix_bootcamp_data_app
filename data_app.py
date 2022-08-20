2f9a43f24191acf5953599c32d8e365e6b0c6bcc
DANNY HUNCA <dhls7@yahoo.com>
2022-08-20 17:16:35 -0400

    My first commit

--- data_app.py
+++ data_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-st.write("hola mundo!")
+
+st.title("CSV Reader")
+file = st.file_uploader('Upload a CSV', type = 'csv')
+if file:
+    df = pd.read_csv(file)
+    st.dataframe(df)
+    st.markdown("---")
+    fig1 = plt.figure(figsize=(10,4))
+    sns.countplot(x='Pclass', data=df)
+
+    st.pyplot(fig1)
+
+    fig2 = plt.figure(figsize=(10,4))
+    sns.boxplot(x='Pclass', 
+                y='Age',
+                data=df
+    )
+
+    st.pyplot(fig2)
 
 
 

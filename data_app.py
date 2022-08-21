1d17e229c919868db58637d2357e3944e66daf6c
DANNY HUNCA <dhls7@yahoo.com>
2022-08-20 22:57:52 -0400

    My first commit data process

--- data_app.py
+++ data_app.py
@@ -14,17 +14,38 @@ if (expected and counted):
     st.dataframe(df_expected)
     st.dataframe(df2_counted)
     st.markdown("---")
-    # fig1 = plt.figure(figsize=(10,4))
-    # sns.countplot(x='Pclass', data=df)
 
-    # st.pyplot(fig1)
+# Data engineering over the two source of information
 
-    # fig2 = plt.figure(figsize=(10,4))
-    # sns.boxplot(x='Pclass', 
-    #             y='Age',
-    #             data=df
-    # )
+    df2_counted = df2_counted.drop_duplicates('RFID')
+    df_B = df2_counted.groupby ("Retail_Product_SKU"). count()[["RFID"]].reset_index().rename(columns={"RFID":"Retail_CCQTY"})
 
-    # st.pyplot(fig2)
+    columnas = ['Retail_Product_Color' , 
+    'Retail_Product_Level1',
+    'Retail_Product_Level1Name',
+    'Retail_Product_Level2Name',
+    'Retail_Product_Level3Name',
+    'Retail_Product_Level4Name',
+    'Retail_Product_Name',
+    'Retail_Product_SKU',
+    'Retail_Product_Size',
+    'Retail_Product_Style',
+    'Retail_SOHQTY'
+    ]
+
+    df_A = df_expected[columnas]
+
+    df_discrepancy = pd.merge(df_A, df_B, how='outer', left_on='Retail_Product_SKU', right_on='Retail_Product_SKU', indicator = True)
+
+    df_discrepancy['Retail_CCQTY'] =  df_discrepancy['Retail_CCQTY'].fillna(0).astype(int)
+    df_discrepancy['Retail_SOHQTY'] =  df_discrepancy['Retail_SOHQTY'].fillna(0).astype(int)
+
+    df_discrepancy['Diff'] = df_discrepancy["Retail_CCQTY"] - df_discrepancy["Retail_SOHQTY"]
+
+    df_discrepancy.loc[df_discrepancy["Diff"]<0, "Unders"] = df_discrepancy["Diff"] * (-1)
+    df_discrepancy['Unders'] =  df_discrepancy['Unders'].fillna(0).astype(int)
+
+    st.dataframe(df_discrepancy)
 
 
+    
